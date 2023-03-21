c36d6eef6f354faf8ee74e67fcb80297bf99ab99
Eslim Daga <eslimdaga@gmail.com>
2023-03-21 08:27:13 -0500

    feat:⭐:La el aside de nuestro reproductor ya está con sus respectivas clases

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -3,142 +3,28 @@ import AsideStyledComponent from "./AsideStyledComponent";
 const Aside = () => {
 	return (
 		<AsideStyledComponent>
-			<div
-				style={{
-					marginTop: "50px",
-					marginBottom: "30px",
-					marginLeft: "40px",
-					marginRight: "40px",
-				}}
-			>
-				<img src="" alt="" style={{ width: "250px", height: "59px" }} />
+			<div className="logo">
+				<img
+					src="images/foxbel-music.png"
+					alt="logo-foxbel-music"
+					width="250"
+					height="59"
+				/>
 			</div>
-			<ul
-				style={{
-					marginLeft: "40px",
-					marginRight: "40px",
-					marginBottom: "30px",
-					listStyle: "none",
-				}}
-			>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "22px",
-						fontWeight: "700",
-						marginBottom: "4px",
-					}}
-				>
-					Mi librería
-				</li>
-				<li
-					style={{
-						color: "#E86060",
-						fontSize: "16px",
-						fontWeight: "700",
-						marginBottom: "8px",
-					}}
-				>
-					Recientes
-				</li>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "16px",
-						fontWeight: "400",
-						marginBottom: "8px",
-					}}
-				>
-					Artistas
-				</li>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "16px",
-						fontWeight: "400",
-						marginBottom: "8px",
-					}}
-				>
-					Album
-				</li>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "16px",
-						fontWeight: "400",
-						marginBottom: "8px",
-					}}
-				>
-					Canciones
-				</li>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "16px",
-						fontWeight: "400",
-						marginBottom: "8px",
-					}}
-				>
-					Estaciones
-				</li>
+			<ul>
+				<li className="title">Mi librería</li>
+				<li className="active">Recientes</li>
+				<li>Artistas</li>
+				<li>Album</li>
+				<li>Canciones</li>
+				<li>Estaciones</li>
 			</ul>
-			<ul
-				style={{
-					marginLeft: "40px",
-					marginRight: "40px",
-					listStyle: "none",
-				}}
-			>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "22px",
-						fontWeight: "700",
-						marginBottom: "4px",
-					}}
-				>
-					Playlist
-				</li>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "16px",
-						fontWeight: "400",
-						marginBottom: "8px",
-					}}
-				>
-					Metal
-				</li>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "16px",
-						fontWeight: "400",
-						marginBottom: "8px",
-					}}
-				>
-					Para bailar
-				</li>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "16px",
-						fontWeight: "400",
-						marginBottom: "8px",
-					}}
-				>
-					Rock 90s
-				</li>
-				<li
-					style={{
-						color: "#fff",
-						fontSize: "16px",
-						fontWeight: "400",
-						marginBottom: "8px",
-					}}
-				>
-					Baladas
-				</li>
+			<ul>
+				<li className="title">Playlist</li>
+				<li>Metal</li>
+				<li>Para bailar</li>
+				<li>Rock 90s</li>
+				<li>Baladas</li>
 			</ul>
 		</AsideStyledComponent>
 	);
