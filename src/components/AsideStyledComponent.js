262dcf116f31cc0db41822efe9df572cd8fab9c9
Eslim Daga <esliim9811@gmail.com>
2023-03-21 01:18:00 -0500

    refactor:🚨:El archivo aside styled components ahora es individual

---
+++ src/components/AsideStyledComponent.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const AsideStyledComponent = styled.aside`
+	background-color: #662323;
+	width: 330px;
+	overflow: auto;
+`;
+
+export default AsideStyledComponent;
