04ed1d3c1a5be1d740b242668eb9a006aff5f689
Eslim Daga <eslimdaga@gmail.com>
2023-03-20 21:05:53 -0500

    feat:⭐:La estructura principal está lista

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-function App() {
-	return <div>Hello World</div>;
-}
+import styled from "styled-components";
+
+const Aside = styled.aside`
+	background-color: #662323;
+	width: 330px;
+	overflow: auto;
+`;
+
+const MainContainer = styled.div`
+	display: flex;
+	height: 100vh;
+`;
+
+const Content = styled.div`
+	width: calc(100% - 330px);
+	overflow: auto;
+`;
+
+const Footer = styled.footer`
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	height: 100px;
+	background-color: #eb5757;
+`;
+
+const App = () => {
+	return (
+		<>
+			<MainContainer>
+				<Aside></Aside>
+				<Content>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				</Content>
+			</MainContainer>
+			<Footer />
+		</>
+	);
+};
 
 export default App;
