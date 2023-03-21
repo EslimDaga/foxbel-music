f9d06d79573df3e85b45d13e8348c48db8af9a63
Eslim Daga <esliim9811@gmail.com>
2023-03-21 00:38:36 -0500

    refactor:🚨:Moví los estilos personalizados del footer a un archivo aparte y así tener un mejor orden.

---
+++ src/components/FooterStyledComponent.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const FooterStyledComponent = styled.footer`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	height: 100px;
+	background-color: #eb5757;
+`;
+
+export default FooterStyledComponent;
