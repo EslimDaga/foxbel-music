2bad04b2b5cfb8ca2c83ae640cfd2d6f8b659152
Eslim Daga <eslimdaga@gmail.com>
2023-03-22 14:25:24 -0500

    update:🔛:Tuve que cambiar el API principal ya que por tiempo no puedo usar el API oficial de Deezer

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "https://cors-anywhere.herokuapp.com/https://api.deezer.com/",
+	baseURL: "https://deezerdevs-deezer.p.rapidapi.com/",
+	params: {
+		q: "grupo 5",
+	},
+	headers: {
+		"X-RapidAPI-Key": "fed3f3d927mshbca68d27409661ap1bc7f0jsn7d6516e80d14",
+		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+	},
 });
 
 export default api;
