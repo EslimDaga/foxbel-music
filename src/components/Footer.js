85ac8c40e4558db1581873cd0240911d2dbcddf5
Eslim Daga <eslimdaga@gmail.com>
2023-03-22 20:48:30 -0500

    feat:⭐:Eh agregado todas las acciones para cada botón excepto el de mutear, ahora tambien es dinámico porque se actualiza cada cambio de canción

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,76 @@
-import FooterStyledComponent from "./FooterStyledComponent";
 import {
 	FaStepBackward,
 	FaPlay,
 	FaStepForward,
 	FaVolumeUp,
+	FaPause,
+	FaVolumeDown,
+	FaVolumeMute,
 } from "react-icons/fa";
+import FooterStyledComponent from "./FooterStyledComponent";
 
-const Footer = () => {
+const Footer = ({
+	isPlaying,
+	currentTrack,
+	handleVolume,
+	volume,
+	handlePlay,
+}) => {
 	return (
 		<FooterStyledComponent>
 			<div className="now-playing-widget">
 				<img
-					src="images/picture.png"
+					src={currentTrack?.album.cover_medium || "images/picture.png"}
 					alt="Album cover"
 					width={100}
 					height={100}
 				/>
 				<div className="now-playing-widget__info">
-					<p>Canción</p>
-					<span>Artista - Album</span>
+					<p>
+						{currentTrack?.title_short.length > 8
+							? currentTrack?.title_short.substring(0, 8) + "..."
+							: currentTrack?.title_short || "Canción"}
+					</p>
+					<span>
+						{currentTrack?.artist.name || "Artista"} -{" "}
+						{currentTrack?.album.title.length > 5
+							? currentTrack?.album.title.substring(0, 5) + "..."
+							: currentTrack?.album.title || "Álbum"}
+					</span>
 				</div>
 			</div>
 			<div className="player-controls">
 				<button className="control-button-skip-back">
 					<FaStepBackward />
 				</button>
-				<button className="control-button-playpause">
-					<FaPlay />
+				<button
+					className="control-button-playpause"
+					onClick={() => {
+						handlePlay();
+					}}
+				>
+					{isPlaying ? <FaPause /> : <FaPlay />}
 				</button>
 				<button className="control-button-skip-forward">
 					<FaStepForward />
 				</button>
 			</div>
 			<div className="volume-bar-widget">
-				<input type="range" min={1} max={800} step={1}></input>
+				<input
+					type="range"
+					min={0}
+					max={100}
+					onChange={handleVolume}
+					value={volume}
+				/>
 				<button>
-					<FaVolumeUp />
+					{volume < 1 ? (
+						<FaVolumeMute />
+					) : volume > 50 ? (
+						<FaVolumeUp />
+					) : (
+						<FaVolumeDown />
+					)}
 				</button>
 			</div>
 		</FooterStyledComponent>
