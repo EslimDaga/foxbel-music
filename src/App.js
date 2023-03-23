2ba86d946088f4e653f504436842b3b50fe702b1
Eslim Daga <eslimdaga@gmail.com>
2023-03-22 20:42:55 -0500

    feat:⭐:Eh agregado las acciones para los botones de play, pause. Tambien eh agregado las acciones para la barra de volumen y por ultimo el buscador funciona correctamente

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { getSongs } from "./services/deezer";
 import styled from "styled-components";
 import Aside from "./components/Aside";
 import Content from "./components/Content";
@@ -9,13 +11,117 @@ const MainContainer = styled.div`
 `;
 
 const App = () => {
+	const [traks, setTraks] = useState([]);
+	const [search, setSearch] = useState("");
+
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [currentTrack, setCurrentTrack] = useState(null);
+	const [audio, setAudio] = useState(null);
+
+	const [volume, setVolume] = useState(100);
+
+	const handleSearch = e => {
+		const { value } = e.target;
+		setSearch(value);
+		getSongs(value).then(response => {
+			setTraks(response);
+		});
+	};
+
+	const handlePlay = () => {
+		setIsPlaying(!isPlaying);
+	};
+
+	const handleCurrentTrack = track => {
+		setCurrentTrack(track);
+	};
+
+	const handleVolume = e => {
+		const { value } = e.target;
+		setVolume(value);
+	};
+
+	useEffect(() => {
+		getSongs().then(response => {
+			setTraks(response);
+		});
+	}, []);
+
+	useEffect(() => {
+		if (isPlaying) {
+			if (audio) {
+				if (currentTrack.preview === audio.src) {
+					audio.play();
+					return;
+				} else {
+					audio.pause();
+					const newAudio = new Audio(currentTrack.preview);
+					setAudio(newAudio);
+					newAudio.play();
+				}
+			} else {
+				const newAudio = new Audio(currentTrack.preview);
+				setAudio(newAudio);
+				newAudio.play();
+			}
+		}
+
+		if (!isPlaying) {
+			if (audio) {
+				audio.pause();
+			}
+		}
+
+		return () => {
+			if (audio) {
+				audio.pause();
+			}
+		};
+	}, [isPlaying]);
+
+	useEffect(() => {
+		if (!isPlaying && currentTrack) {
+			const newAudio = new Audio(currentTrack.preview);
+			setAudio(newAudio);
+			setIsPlaying(true);
+		}
+	}, [currentTrack]);
+
+	useEffect(() => {
+		if (audio) {
+			audio.addEventListener("ended", () => {
+				setIsPlaying(false);
+			});
+		}
+	}, [audio]);
+
+	useEffect(() => {
+		if (audio) {
+			audio.volume = volume / 100;
+		}
+	}, [volume, audio]);
+
 	return (
 		<>
 			<MainContainer>
 				<Aside />
-				<Content />
+				<Content
+					traks={traks}
+					search={search}
+					handleSearch={handleSearch}
+					handlePlay={handlePlay}
+					isPlaying={isPlaying}
+					handleCurrentTrack={handleCurrentTrack}
+					currentTrack={currentTrack}
+				/>
 			</MainContainer>
-			<Footer />
+			<Footer
+				currentTrack={currentTrack}
+				isPlaying={isPlaying}
+				handlePlay={handlePlay}
+				handleVolume={handleVolume}
+				volume={volume}
+			/>
 		</>
 	);
 };
