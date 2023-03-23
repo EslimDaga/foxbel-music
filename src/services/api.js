e89b4b8f7842dae9214514ed252e2fbb3dd91d59
Eslim Daga <eslimdaga@gmail.com>
2023-03-22 20:49:36 -0500

    update:🔛:Removí algo que no me parecia necesario, el params en el axios create ya lo declaro en el deezer.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
 	baseURL: "https://deezerdevs-deezer.p.rapidapi.com/",
-	params: {
-		q: "grupo 5",
-	},
 	headers: {
 		"X-RapidAPI-Key": "fed3f3d927mshbca68d27409661ap1bc7f0jsn7d6516e80d14",
 		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
